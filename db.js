c4418a9b56d7b27814ff3ce2d698b245b5b44b17
kwonsoonha <jh1man@naver.com>
2025-12-08 20:39:44 +0900

    no message

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',               // 설치할 때 설정한 PostgreSQL 사용자
-  host: 'localhost',              // 내 컴퓨터니까 localhost
-  database: 'postgres',           // pgAdmin에서 복원한 데이터베이스 이름
-  password: '2101',  // 이거 까먹으면 안됨
-  port: 5433                      // 설치할 때 설정한 포트 (기본값이 5432인데 넌 5433일 수도 있어)
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 module.exports = pool;
